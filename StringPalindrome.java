1c6b013c4cfe73f11f3ee7e442200bee04237859
MaheshTekumani <41639058+MaheshTekumani@users.noreply.github.com>
2018-08-07 22:16:55 +0530

    Update StringPalindrome.java

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,18 +1,13 @@
 package StringPrograms;
-
 import java.util.Scanner;
-
 public class StringPalindrome {
-
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) {	
 		Scanner sc=new Scanner(System.in);
 		System.out.println("Enter The Sentence");
 		String s=sc.nextLine();
 		boolean s1=checkPolin(s);
 		System.out.println(s1);
 	}
-
 	private static boolean checkPolin(String s) {
 		char[] ch=s.toCharArray();
 		int i=0;
@@ -24,10 +19,5 @@ public class StringPalindrome {
 			}
 			i++;
 		}return true;
-		
-		
-		
-		
-	}
-
+}
 }
